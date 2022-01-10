f6c18467972a11f7c22bf02a96cd1e55830cb9d1
FernandoGM2701 <fernandogm2701@gmail.com>
2022-01-10 10:54:14 -0500

    Array o arreglos

---
+++ js/08.js
@@ -0,0 +1,20 @@
+// Array o Arreglos
+
+const lenguajes_programacion = [
+    "React",
+    "JavaScript",
+    4,
+    "Go",
+    "Java"
+]
+
+console.log(lenguajes_programacion)
+console.table(lenguajes_programacion)
+
+console.log(lenguajes_programacion[1])
+
+ //Cantidad de elementos
+ console.log(lenguajes_programacion.length) 
+
+ //Mostrar elementos como string
+ console.log((lenguajes_programacion.toString()))
