2aa4e8dce1c68b3d349e33535c816882a7c65c2b
FernandoGM2701 <fernandogm2701@gmail.com>
2022-01-11 14:56:01 -0500

    Destructuring de arrays

---
+++ js/10.js
@@ -0,0 +1,6 @@
+// Destructuring de arrays
+const tecnologias = [ "HTML", "CSS", "JavaScript", "React", "Node.js"]
+
+const [ , , , , var5] = tecnologias //El destructuring se hace a través de comas
+
+console.log(var5)
