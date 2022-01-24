4cc4d8c01522a55d33d5fc0032b902ca71c5946b
FernandoGM2701 <fernandogm2701@gmail.com>
2022-01-23 19:55:09 -0500

    Condicionales - if

---
+++ js/17.js
@@ -0,0 +1,20 @@
+// Condicionales
+
+const disponible = 4000
+const retirar = 200000
+
+if( disponible>retirar ){
+    // Se cumple la condición y se ejecuta el código
+    console.log("Si se puede retirar")
+} else {
+    // no se cumple la condición, ejecutar este código
+    console.log('No puedes retirar')
+}
+
+
+/**
+ *  > - Mayor Que
+ *  < - Menor que
+ *  >=  Mayor o igual
+ *  <= Menor o igual
+ */
