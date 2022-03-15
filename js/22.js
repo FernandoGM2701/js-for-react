e64d3c29188404cf24b32c58d88eddd7ca9de371
FernandoGM2701 <fernandogm2701@gmail.com>
2022-03-15 00:44:47 -0500

    Selectores del DOM - querySelector

---
+++ js/22.js
@@ -0,0 +1,9 @@
+//Selectores del DOM
+const h2 = document.querySelector('.h11');
+const p = document.querySelector(".pp");
+console.log(h2);
+console.log(h2.textContent);
+
+console.log(p);
+console.log(p.textContent);
+console.log(p.id);
