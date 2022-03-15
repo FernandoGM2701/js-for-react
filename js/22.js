b0f2fc844ce553160c7f829f4ed7bb31a7fa3d23
FernandoGM2701 <fernandogm2701@gmail.com>
2022-03-15 00:56:43 -0500

    Selectores del DOM - querySelectorAll

--- js/22.js
+++ js/22.js
@@ -1,9 +1,19 @@
 //Selectores del DOM
 const h2 = document.querySelector('.h11');
 const p = document.querySelector(".pp");
-console.log(h2);
+/*console.log(h2);
 console.log(h2.textContent);
 
 console.log(p);
 console.log(p.textContent);
-console.log(p.id);
+console.log(p.id);*/
+
+const enlaces = document.querySelectorAll(".navegacion a");
+
+console.log(enlaces);
+
+console.log(enlaces.length);
+
+for(let i=0; i<enlaces.length; i++){
+    console.log(enlaces[i].textContent);
+}
