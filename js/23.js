3e38bad6d794baa3149b873570498d254bae9ae8
FernandoGM2701 <fernandogm2701@gmail.com>
2022-03-23 08:55:51 -0500

    Manipular HTML con JS

---
+++ js/23.js
@@ -0,0 +1,9 @@
+// Manipular elemento HTML con JS
+
+const heading = document.querySelector(".header p");
+// heading.textContent = "SE CAMBIA EL HEADING"
+console.log(heading.textContent);
+
+const input = document.querySelector(".formulario #nombre");
+input.value = "CAMBIO ACÁ";
+console.log(input);
