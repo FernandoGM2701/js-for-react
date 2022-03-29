a6a6ef3f13f0ec53f9a57fcca64fb872432d9757
unknown <fernandogm2701@gmail.com>
2022-03-28 22:48:10 -0500

    Imports y Exports

---
+++ js/28.js
@@ -0,0 +1,5 @@
+import { sumar, restar } from "./funciones.js"
+
+const resultSuma = sumar(10,20);
+const resultResta = restar(50,3);
+console.log(`La suma es: ${resultSuma} y la resta es: ${resultResta}`);
